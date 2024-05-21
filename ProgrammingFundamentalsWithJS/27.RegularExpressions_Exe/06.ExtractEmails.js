1b540fd9bda4d7c7abe823b67cfc3ee5caea11d8
Savina Popova <savina.popova@yahoo.com>
2024-05-21 12:06:19 +0300

    Files added

---
+++ ProgrammingFundamentalsWithJS/27.RegularExpressions_Exe/06.ExtractEmails.js
@@ -0,0 +1,9 @@
+function solve(text) {
+    let pattern = /(?<= |^)([A-Za-z0-9]+(?:[\.\-\_][A-Za-z0-9]+)*@[A-Za-z]+(?:[\-]+[A-Za-z]+)*\.[A-Za-z]+(?:[\-]+[A-Za-z]+)*(?:\.[A-Za-z]+(?:[\-]+[A-Za-z]+)*)*)/gm;
+
+        let match = text.match(pattern);
+
+        if (match !== null) {
+            console.log(match.join("\n"));
+        }
+}
