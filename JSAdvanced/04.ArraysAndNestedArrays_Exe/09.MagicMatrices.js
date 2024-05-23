a573f683ddb97643ff1010fa73ec3ffa89cb92b4
Savina Popova <savina.popova@yahoo.com>
2024-05-23 13:48:56 +0300

    Files added

---
+++ JSAdvanced/04.ArraysAndNestedArrays_Exe/09.MagicMatrices.js
@@ -0,0 +1,28 @@
+function solve(matrix) {
+
+    let sums = new Set();
+
+    for (let row = 0; row < matrix.length; row++) {
+        let sum = matrix[row].reduce((a,b) => a +b, 0);
+        sums.add(sum);
+        }
+    let turned = [];
+
+
+    for (let row = 0; row < matrix.length; row++) {
+        for (let col = 0; col < matrix[row].length; col++) {
+            if (turned[col] === undefined) {
+                turned[col] = [];
+            }
+            turned[col][matrix.length - 1 - row] = matrix[row][col];
+        }
+
+}
+
+
+    for (let row = 0; row < turned.length; row++) {
+        let sum = turned[row].reduce((a, b) => a + b, 0);
+        sums.add(sum);
+    }
+    console.log(sums.size === 1);
+}
