562a9e56dd2191a345e0119505a07c0cd580216c
Savina Popova <savina.popova@yahoo.com>
2024-06-07 21:46:35 +0300

    Files added

--- JSAdvanced/08.DOMIntroduction_Exe/08.GenerateReport/report.js
+++ JSAdvanced/08.DOMIntroduction_Exe/08.GenerateReport/report.js
@@ -1,3 +1,42 @@
 function generateReport() {
-    //TODO
+
+    let headers = Array.from(document.querySelector("thead tr").children);
+
+    let properties = headers
+        .map(h => h.querySelector("[type = 'checkbox']"))
+        .map(c => c.getAttribute("name"));
+
+    let checkedIndexes = [];
+
+    for (let i = 0; i < headers.length; i++) {
+
+        let checkbox = headers[i].querySelector("[type = 'checkbox']");
+        if (checkbox.checked) {
+            checkedIndexes.push(i);
+        }
+    }
+
+    let rows = Array.from(document.querySelector("tbody").children);
+    let objects ={};
+
+    for (let row = 0; row < rows.length; row++) {
+
+        let cols = Array.from(rows[row].children)
+            .map(c => c.textContent.trim());
+
+        objects[row] = {};
+
+        for (let i = 0; i < cols.length; i++) {
+            if (checkedIndexes.includes(i)) {
+                objects[row][properties[i]] = cols[i];
+            }
+        }
+    }
+
+    let output = JSON.stringify(Object.values(objects), null, 2);
+
+    let outputArea = document.getElementById("output");
+
+    outputArea.value = output;
+
 }
