caf6f21bc9bb2f91aa51c3edc0965449432e76d0
Savina Popova <savina.popova@yahoo.com>
2024-06-08 07:42:26 +0300

    Files added

--- JSAdvanced/08.DOMIntroduction_Exe/09.NumberConvertor/solution.js
+++ JSAdvanced/08.DOMIntroduction_Exe/09.NumberConvertor/solution.js
@@ -1,4 +1,28 @@
 function solve() {
+    let input = document.getElementById('input');
+    let selectMenuTo = document.getElementById('selectMenuTo');
+    let result = document.getElementById('result');
 
-    //TODO...
+    let binaryOption = document.createElement('option');
+    binaryOption.textContent = 'Binary';
+    binaryOption.value = 'binary';
+    selectMenuTo.appendChild(binaryOption);
+
+    let hexOption = document.createElement('option');
+    hexOption.textContent = 'Hexadecimal';
+    hexOption.value = 'hexadecimal';
+    selectMenuTo.appendChild(hexOption);
+
+    let convertBtn = document.querySelector('button');
+    convertBtn.addEventListener('click', () => {
+        let number = Number(input.value);
+        let resultValue = '';
+        if (selectMenuTo.value === 'binary') {
+            resultValue = number.toString(2);
+        } else if (selectMenuTo.value === 'hexadecimal') {
+            resultValue = number.toString(16).toUpperCase();
+        }
+
+        result.value = resultValue;
+    });
 }
