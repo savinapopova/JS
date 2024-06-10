68a819ea08b8636b8a6444022250ed6fd818f157
Savina Popova <savina.popova@yahoo.com>
2024-06-10 18:52:37 +0300

    Files added

--- JSAdvanced/09.DOMManipulationAndEvents_Lab/05.HighlightActive/app.js
+++ JSAdvanced/09.DOMManipulationAndEvents_Lab/05.HighlightActive/app.js
@@ -1,3 +1,17 @@
 function focused() {
-    console.log('TODO:...');
+    let inputs = Array.from(document.querySelectorAll('input'))
+        .map(i => {
+            i.addEventListener('focus', addClass);
+            i.addEventListener('blur', removeClass);
+        });
+
+    function addClass(event) {
+        event.target.parentElement.classList.add('focused');
+    }
+
+    function removeClass(event) {
+        event.target.parentElement.classList.remove('focused');
+    }
+
+
 }
