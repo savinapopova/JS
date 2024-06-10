f43a52fca6fe107f8e948f4775fec287be3ef1b8
Savina Popova <savina.popova@yahoo.com>
2024-06-10 13:49:08 +0300

    Files added

--- JSAdvanced/09.DOMManipulationAndEvents_Lab/02.DeleteFromTable/app.js
+++ JSAdvanced/09.DOMManipulationAndEvents_Lab/02.DeleteFromTable/app.js
@@ -1,3 +1,23 @@
 function deleteByEmail() {
-    console.log('TODO:...');
+    let inputField = document.querySelector('[name = "email"]');
+    let email = inputField.value;
+    let rows = Array.from(document.querySelector("tbody").children);
+
+    let deleted = false;
+
+    for (let row of rows) {
+        if (row.children[1].textContent === email) {
+            row.remove();
+            inputField.value = "";
+            deleted = true;
+        }
+    }
+
+    let result = document.getElementById("result");
+
+    if (deleted) {
+        result.textContent = "Deleted.";
+    } else {
+        result.textContent = "Not found.";
+    }
 }
