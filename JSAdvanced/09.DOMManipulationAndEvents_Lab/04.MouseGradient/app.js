9e3919acd68df757711fe3f94e8a3950f803b82d
Savina Popova <savina.popova@yahoo.com>
2024-06-10 18:33:30 +0300

    Files added

--- JSAdvanced/09.DOMManipulationAndEvents_Lab/04.MouseGradient/app.js
+++ JSAdvanced/09.DOMManipulationAndEvents_Lab/04.MouseGradient/app.js
@@ -1,3 +1,22 @@
 function attachGradientEvents() {
-    console.log('TODO:...');
+    let element = document.getElementById("gradient");
+    let result = document.getElementById("result");
+
+    element.addEventListener('mousemove', showPercent);
+    element.addEventListener('mouseout', hidePercent);
+
+    function showPercent(event) {
+        let width = element.clientWidth;
+        let offset = event.offsetX;
+        let part = offset / (width);
+        let percent = Math.trunc(part * 100);
+
+        result.textContent = `${percent}%`;
+    }
+
+    function hidePercent() {
+        result.textContent = '';
+    }
+
+
 }
