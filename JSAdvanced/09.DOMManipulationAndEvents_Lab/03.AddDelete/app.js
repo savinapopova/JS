c784752cf81c39bc183a77b5ad76d680334d8b79
Savina Popova <savina.popova@yahoo.com>
2024-06-10 13:58:23 +0300

    Files added

--- JSAdvanced/09.DOMManipulationAndEvents_Lab/03.AddDelete/app.js
+++ JSAdvanced/09.DOMManipulationAndEvents_Lab/03.AddDelete/app.js
@@ -1,3 +1,25 @@
 function addItem() {
-    //TODO...
+    let inputField = document.getElementById("newItemText");
+    let text = inputField.value;
+    if (text.trim().length === 0) {
+        inputField.value = "";
+        return;
+    }
+    let list = document.getElementById("items");
+    let liElement = document.createElement('li');
+    liElement.textContent = text;
+    list.appendChild(liElement);
+    inputField.value = "";
+
+    let deleteLink = document.createElement("a");
+    deleteLink.href = '#';
+    deleteLink.textContent = "[Delete]";
+    liElement.appendChild(deleteLink);
+
+    deleteLink.addEventListener('click', deleteItem);
+
+    function deleteItem(event) {
+        let item = event.target.parentElement;
+        item.remove();
+    }
 }
