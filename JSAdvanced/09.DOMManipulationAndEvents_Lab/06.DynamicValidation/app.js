28d7da1c3d561d30ffc7752c72cdba980c207753
Savina Popova <savina.popova@yahoo.com>
2024-06-11 12:12:54 +0300

    Files added

--- JSAdvanced/09.DOMManipulationAndEvents_Lab/06.DynamicValidation/app.js
+++ JSAdvanced/09.DOMManipulationAndEvents_Lab/06.DynamicValidation/app.js
@@ -1,3 +1,17 @@
 function validate() {
-    console.log('TODO:...');
+    let inputField = document.getElementById('email');
+
+    inputField.addEventListener('change', validateEmail);
+
+    function validateEmail() {
+        let email = inputField.value;
+
+        let pattern = /^[a-z]+@[a-z]+\.[a-z]+$/gm;
+
+        if (!email.match(pattern)) {
+            inputField.classList.add('error');
+        } else {
+            inputField.classList.remove('error');
+        }
+    }
 }
