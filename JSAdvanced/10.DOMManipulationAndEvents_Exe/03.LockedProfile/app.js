04bbc8b4bbf2a4d2215a33beacf58a7c905ceb0e
Savina Popova <savina.popova@yahoo.com>
2024-06-12 14:46:37 +0300

    Files added

--- JSAdvanced/10.DOMManipulationAndEvents_Exe/03.LockedProfile/app.js
+++ JSAdvanced/10.DOMManipulationAndEvents_Exe/03.LockedProfile/app.js
@@ -1,3 +1,25 @@
 function lockedProfile() {
-    console.log('TODO...')
+    let buttons = Array.from(document.querySelectorAll('button'));
+    buttons
+        .forEach(b => b.addEventListener('click', manageInfo));
+
+    function manageInfo(event) {
+        let btn = event.target;
+        let lock = btn.parentElement
+            .querySelector('[value = "lock"]');
+
+        if (lock.checked) {
+            return;
+        }
+        let hiddenInfo = btn
+            .parentElement
+            .querySelector('div');
+        if (btn.textContent === "Show more") {
+            hiddenInfo.style.display = 'block';
+            btn.textContent = "Hide it";
+        } else {
+            hiddenInfo.style.display = 'none';
+            btn.textContent = "Show more";
+        }
+    }
 }
--- JSAdvanced/10.DOMManipulationAndEvents_Exe/04.FillDropdown/dropdown.js
+++ JSAdvanced/10.DOMManipulationAndEvents_Exe/04.FillDropdown/dropdown.js
@@ -1,3 +1,20 @@
 function addItem() {
-    console.log('TODO:...');
+
+    let menu = document.getElementById('menu');
+    let textInput = document.getElementById('newItemText');
+    let valueInput = document.getElementById('newItemValue');
+
+    let text = textInput.value;
+    let value = valueInput.value;
+
+    if (text.trim().length === 0 || value.trim().length === 0) {
+        return;
+    }
+
+    let option = document.createElement('option');
+    option.textContent = text;
+    option.value = value;
+    menu.appendChild(option);
+    textInput.value = '';
+    valueInput.value = '';
 }
