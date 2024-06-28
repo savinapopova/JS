ac1b2fcb88b0419226aa6a1760ad8b02f24ad57d
Savina Popova <savina.popova@yahoo.com>
2024-06-28 18:57:34 +0300

    Files added

---
+++ JSAdvanced/15.ClassesAndAttributes_Lab/03.Circle.js
@@ -0,0 +1,19 @@
+class Circle {
+    radius;
+
+    constructor(radius) {
+        this.radius = radius;
+    }
+
+    get diameter() {
+        return this.radius * 2;
+    }
+
+    set diameter(diameter) {
+        this.radius = diameter / 2;
+    }
+
+    get area() {
+        return Math.PI * this.radius ** 2;
+    }
+}
