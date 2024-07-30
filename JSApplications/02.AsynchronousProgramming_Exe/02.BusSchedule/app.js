136031fa22440b8d03ccb43916435e4626753cdc
Savina Popova <savina.popova@yahoo.com>
2024-07-30 15:28:50 +0300

    Files added

--- JSApplications/02.AsynchronousProgramming_Exe/02.BusSchedule/app.js
+++ JSApplications/02.AsynchronousProgramming_Exe/02.BusSchedule/app.js
@@ -1,11 +1,34 @@
 function solve() {
+    let busId = 'depot';
+    let url = `http://localhost:3030/jsonstore/bus/schedule/`;
+    let stopField = document.querySelector(`#info span`);
+    let departBtn = document.getElementById('depart');
+    let arriveBtn = document.getElementById('arrive');
+    let  data;
 
-    function depart() {
-        console.log('Depart TODO...');
+
+    async function depart() {
+        try {
+            let response = await fetch(url + busId);
+            data = await response.json();
+            if (!response.ok || !data.name || !data.next) {
+                throw new Error();
+            }
+            console.log(data);
+            busId = data.name;
+            stopField.textContent = `Next stop ${busId}`;
+            departBtn.disabled = true;
+            arriveBtn.disabled = false;
+        } catch (error) {
+            stopField.textContent = 'Error';
+        }
     }
 
     function arrive() {
-        console.log('Arrive TODO...');
+        stopField.textContent = `Arrived at ${busId}`;
+        busId = data.next;
+        arriveBtn.disabled = true;
+        departBtn.disabled = false;
     }
 
     return {
