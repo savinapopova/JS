3bd744ed7116896d0419cac4ffc310e25fb11d4f
Savina Popova <savina.popova@yahoo.com>
2024-11-19 17:58:28 +0200

    Files added

--- JSApplications/08.ClientSideRendering_Exe/04.Fill-Dropdown/dropdown.js
+++ JSApplications/08.ClientSideRendering_Exe/04.Fill-Dropdown/dropdown.js
@@ -1,3 +1,37 @@
-function addItem() {
-    console.log('TODO:...');
+import { html, render } from './node_modules/lit-html/lit-html.js';
+import { get, post } from './request.js';
+
+const url = 'http://localhost:3030/jsonstore/advanced/dropdown';
+const select = document.getElementById('menu');
+const form = document.querySelector('form');
+form.addEventListener("submit", addItem);
+
+await loadOptions();
+
+async function loadOptions() {
+
+
+     let data = await get(url);
+     console.log(data);
+
+     let optionsTemplate = Object.values(data)
+         .map(d => html`<option value="${d._id}">${d.text}</option>`);
+     render(optionsTemplate, select);
+
 }
+
+async function addItem(event) {
+    event.preventDefault();
+const input = document.getElementById('itemText');
+let text = input.value.trim();
+if (text === '') {
+    alert("No text inserted");
+    return;
+}
+    await post(url, {text});
+    input.value = '';
+    await loadOptions();
+
+}
+
+
